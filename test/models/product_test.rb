e332c618189868c00bac07898e6d50cffc9e6ac9
Ogada Moses <ogadamoses59@gmail.com>
2022-10-19 17:16:57 +0300

    Add Store Controller and Styling

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -34,14 +34,22 @@ class ProductTest < ActiveSupport::TestCase
     test "image_url" do
       ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
         http://a.b.c/x/y/z/fred.gif }
-        bad = %w{ fred.doc fred.gif/more fred.gif.more }
+      bad = %w{ fred.doc fred.gif/more fred.gif.more }
         ok.each do |image_url|
           assert new_product(image_url).valid?,
           #'{image_url} shouldn't be valid"
-    end
+        end 
+      end       
 
     bad.each do |image_url|
       assert new_product(image_url).invalid?,
       #{image_url} shouldn't be valid"
+    end
+
+    test "Product is not valid without a unique title" do
+      product = Product.new(title:  products(:ruby).title, description: "yyy", price: 1, 
+      image_url: "moses.png")
+      assert product.invalid?
+      assert_equal [" has already been taken"], product.errors [:title]
     end
 end
